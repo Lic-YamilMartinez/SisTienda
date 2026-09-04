13bf47f5798973d3930fd4a160748cc88f9f8914
Lic-YamilMartinez <carlosyamil81@gmail.com>
2026-09-04 12:09:51 -0300

    feat: add domain validation exception

---
+++ app-core/src/main/java/py/sistienda/core/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package py.sistienda.core.exception;
+
+public final class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
