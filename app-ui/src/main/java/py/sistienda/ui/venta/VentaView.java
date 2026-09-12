099f0e9b8b60eee6845430e9417d6d77364f5cc5
Lic-YamilMartinez <carlosyamil81@gmail.com>
2026-09-12 07:40:25 -0300

    fix: import JavaFX node for credit action state

--- app-ui/src/main/java/py/sistienda/ui/venta/VentaView.java
+++ app-ui/src/main/java/py/sistienda/ui/venta/VentaView.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
